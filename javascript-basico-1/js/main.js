17f89a637185bdca96c98d75555a1d0eee6209ad
unknown <alexnobrecruz@yahoo.com.br>
2021-02-16 21:12:17 -0300

    Criação dos arquivos js/javascript com exemplos

---
+++ javascript-basico-1/js/main.js
@@ -0,0 +1,19 @@
+var nome = "Alex";
+var idade = 34;
+var idade2 = 20;
+var frase = "França possui melhor time do mundo";
+//   EXEMPLOS DE ALERT
+
+//alert("Seja bem vindo "+ nome +" possui idade: "+idade);
+//alert(idade+idade2);//Soma o valor
+                      //se a idade estivesse atribuida entre aspas "34" e "20", entederia que era string e iria concatenar as informações
+
+
+//Exibindo informações no consolde do navegador
+
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase);
+console.log(frase.replace("França","Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
