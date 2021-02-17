769cd08c67fef734dc604591ee0cee1895af9f0e
unknown <alexnobrecruz@yahoo.com.br>
2021-02-16 21:33:13 -0300

    Exemplos comando: exibir informações no console, listas, array de listas, e outros comandos. Ver arquivo main.js

--- javascript-basico-1/js/main.js
+++ javascript-basico-1/js/main.js
@@ -2,18 +2,51 @@ var nome = "Alex";
 var idade = 34;
 var idade2 = 20;
 var frase = "França possui melhor time do mundo";
+/* *********************************** */
 //   EXEMPLOS DE ALERT
-
+/* *********************************** */
 //alert("Seja bem vindo "+ nome +" possui idade: "+idade);
 //alert(idade+idade2);//Soma o valor
                       //se a idade estivesse atribuida entre aspas "34" e "20", entederia que era string e iria concatenar as informações
 
 
+/* *********************************** */
 //Exibindo informações no consolde do navegador
+/* *********************************** */
 
-console.log(nome);
+/*console.log(nome);
 console.log(idade+idade2);
 console.log(frase);
 console.log(frase.replace("França","Brasil"));
 console.log(frase.toUpperCase());
-console.log(frase.toLowerCase());
+console.log(frase.toLowerCase());*/
+
+/* *********************************** */
+//Exemplos de lista
+/* *********************************** */
+
+/*var lista = ["maça", "pêra", "laranja"];
+console.log(lista);
+lista.push("banana");
+console.log(lista);
+lista.pop("banana");
+console.log(lista);
+console.log(lista.length);
+console.log(lista[1]);
+console.log(lista.toString());
+console.log(lista.toString()[0]);
+console.log(lista.join(" | "));*/
+
+/* *********************************** */
+//Array de listas
+/* *********************************** */
+
+/*var fruta = {nome:"Maça", cor:"Vermelha"};
+console.log(fruta);
+console.log(fruta.nome+" - "+fruta.cor);
+
+var frutas = [{nome:"Maça", cor:"Vermelha"},
+              {nome:"Uva", cor:"Roxa"}
+            ];
+console.log(frutas);            
+console.log(frutas[0].nome);*/
